a944b1ceabc1c7293966ccf916ab5ab306b62513
Susmitha Anandarao <saphire.susmitha@gmail.com>
2020-04-20 17:52:58 -0500

    Update weather result

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location')
         } else {
             const current = body.current
-            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
+            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out. The humidity is ${current.humidity}%.`)
         }
     })
 }
