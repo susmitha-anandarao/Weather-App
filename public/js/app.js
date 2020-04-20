7eafbbaeb07014bc68cdf53f726e84998f0e5b7d
Susmitha Anandarao <saphire.susmitha@gmail.com>
2020-04-20 17:34:04 -0500

    Update client side js

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-    const url = 'git status/weather?address=' + location
+    const url = '/weather?address=' + location
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
